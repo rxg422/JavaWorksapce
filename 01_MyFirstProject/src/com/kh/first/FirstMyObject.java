a24ef38f9772e2880766b54b96ad7e79d90d4010
user1 <user1@DESKTOP-OK6S3LO>
2025-12-19 16:42:42 +0900

    2일차 수정사항

--- 01_MyFirstProject/src/com/kh/first/FirstMyObject.java
+++ 01_MyFirstProject/src/com/kh/first/FirstMyObject.java
@@ -4,7 +4,7 @@ package com.kh.first;
 import java.util.Date;
 
 public class FirstMyObject {
-	// 자바 실행을 위해 main() 필수
+	// 자바 실행을 위해 main() 필수 //
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println(new Date());
