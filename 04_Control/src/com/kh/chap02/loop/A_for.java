6860c29565bb58b1565297cc982bee92d83ba218
user1 <user1@DESKTOP-OK6S3LO>
2025-12-22 17:09:34 +0900

    2025-12-22

--- 04_Control/src/com/kh/chap02/loop/A_for.java
+++ 04_Control/src/com/kh/chap02/loop/A_for.java
@@ -12,4 +12,56 @@ public class A_for {
 		}
 	}
 	
+	public void method2() {
+		for(int i=0; i<5; i++) {
+			System.out.print((i+1) + " \n");
+		}
+	}
+	
+	public void method3() {
+		for(int i=1; i<10; i+=2) {
+			System.out.print(i+" \n");
+		}
+	}
+	
+	public void method4() {
+		int sum = 0;
+		
+		for(int i=1; i<=10; i++) {
+			sum += i;
+		}
+		
+		System.out.printf("1~10의 합 : %d\n", sum);
+	}
+	
+	public void method5() {
+		int n = (int)(Math.random()*10+1);
+		int sum = 0;
+		
+		for(int i=1; i<=n; i++) {
+			sum += i;
+		}
+		
+		
+		System.out.printf("1부터 %d까지의 합 : %d", n, sum);
+	}
+	
+	public void method6() {
+		String str = "Hello Friends";
+		
+		for(int i=0; i<str.length(); i++) {
+			System.out.print("\'"+str.charAt(i)+"\',");
+		}
+	}
+	
+	public void method7() {
+		for(int i=1; i<=9; i++) {
+			System.out.printf("%d단\n", i);
+			for(int j=1; j<=9; j++) {
+				System.out.printf("%d*%d=%d ", i, j, i*j);
+			}
+			System.out.println("\n");
+		}
+	}
+	
 }
--- 04_Control/src/com/kh/chap02/loop/Run.java
+++ 04_Control/src/com/kh/chap02/loop/Run.java
@@ -5,7 +5,13 @@ public class Run {
 	public static void main(String[] args) {
 		A_for a = new A_for();
 		
-		a.method1();
+//		a.method1();
+//		a.method2();
+//		a.method3();
+//		a.method4();
+//		a.method5();
+//		a.method6();
+		a.method7();
 		
 		a.sc.close();
 	}
