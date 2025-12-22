096e1c74b1465bf875013633e2832059b7cda831
user1 <user1@DESKTOP-OK6S3LO>
2025-12-22 11:50:07 +0900

    Day3

--- 04_Control/src/com/kh/chap01/condition/B_switch.java
+++ 04_Control/src/com/kh/chap01/condition/B_switch.java
@@ -8,7 +8,25 @@ public class B_switch {
 	Scanner sc = new Scanner(System.in);
 	
 	public void method1() {
-		System.out.println("값 : ");
+		System.out.print("값 : ");
+		int value = sc.nextInt();
+		
+		if(value==1) {
+			System.out.println("빨간불");
+		}
+		else if(value==2) {
+			System.out.println("파란불");
+		}
+		else if(value==3) {
+			System.out.println("초록불");
+		}
+		else {
+			System.out.println("잘못된 값을 입력하였습니다.");
+		}
+	}
+	
+	public void method2() {
+		System.out.print("값 : ");
 		int value = sc.nextInt();
 		
 		switch(value) {
@@ -24,6 +42,10 @@ public class B_switch {
 		default:
 			System.out.println("잘못된 값을 입력하였습니다.");
 		}
+	}
+	
+	public void method3() {
+		
 	}
 	
 }
--- 04_Control/src/com/kh/chap01/condition/Run.java
+++ 04_Control/src/com/kh/chap01/condition/Run.java
@@ -4,15 +4,19 @@ public class Run {
 
 	public static void main(String[] args) {
 		A_if a = new A_if();
-		B_switch = new B_switch();
+		B_switch b = new B_switch();
 		
 //		a.method1();
 //		a.method3();
 //		a.method4();
 //		a.method5();
-		a.method6();
+//		a.method6();
+		
+//		b.method1();
+		b.method2();
 		
 		a.sc.close();
+		b.sc.close();
 	}
 
 }
