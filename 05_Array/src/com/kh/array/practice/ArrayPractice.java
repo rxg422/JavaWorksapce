d5e59363a3704feea9a6b75ec92780618d38e947
user1 <user1@DESKTOP-OK6S3LO>
2025-12-24 16:01:17 +0900

    2025-12-24

---
+++ 05_Array/src/com/kh/array/practice/ArrayPractice.java
@@ -0,0 +1,248 @@
+package com.kh.array.practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayPractice {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public void practice01() {
+		int arr[] = new int[10];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = arr.length - i;
+			System.out.printf("%d ", arr[i]);
+		}
+		System.out.println();
+	}
+	
+	public void practice02() {
+		int arr[], n;
+		
+		System.out.print("양의 정수 : ");
+		n = sc.nextInt();
+		
+		arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = i+1;
+			System.out.printf("%d ", arr[i]);
+		}
+		System.out.println();
+	}
+	
+	public void practice03() {
+		int arr[] = new int[10];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = (int)(Math.random() * 10 + 1);
+			System.out.printf("%d ", arr[i]);
+		}
+		System.out.println();
+	}
+	
+	public void practice04() {
+		char week[] = {'월', '화', '수', '목', '금', '토', '일'};
+		int n;
+		
+		System.out.print("0~6 사이 숫자 입력 : ");
+		n = sc.nextInt();
+		
+		if(n<0 || n>6) {
+			System.out.println("잘못 입력하셨습니다.");
+			return;
+		}
+		
+		System.out.printf("%c요일", week[n]);
+	}
+	
+	public void practice05() {
+		int n, arr[], tmp, sum=0;
+		
+		System.out.print("정수 : ");
+		n = sc.nextInt();
+		
+		arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			System.out.printf("배열 %d번째 인덱스에 넣을 값 : ", i);
+			tmp = sc.nextInt();
+			sum += tmp;
+			arr[i] = tmp;
+		}
+		
+//		for(int i=0; i<n; i++) {
+//			System.out.printf("%d ", arr[i]);
+//		}
+		
+		System.out.println(Arrays.toString(arr));
+		
+		System.out.printf("\n총합 : %d", sum);
+	}
+	
+	public void practice06() {
+		
+		String str;
+		char arr[], arr2[];
+		
+		// 1. 문자열 입력 받기
+		System.out.print("문자열 : ");
+		str = sc.nextLine();
+		
+		// 2. 중첩을 제외하고 배열에 저장
+		arr = str.toCharArray();
+		arr2 = new char[arr.length];
+		int cnt = 0;
+		
+		
+		System.out.print("문자열에 있는 문자 : ");
+		for(int i=0; i<str.length(); i++) {
+			for(int j=0; j<=i; j++) {
+				if(i==j) {
+					System.out.printf("%c ", arr[i]);
+				}
+				if(arr[i] == arr[j]) {
+					break;
+				}
+			}
+		}
+		System.out.println("\n문자 개수 : " + cnt);
+	}
+	
+	public void practice07() {
+		int cnt = 0;
+		// 1. 문자열을 입력받아
+		System.out.print("문자열 : ");
+		String str = sc.nextLine();
+		// 2. 문자를 배열에 넣고
+		char arr[] = str.toCharArray();
+		// 3. 검색할 문자가
+		System.out.print("문자 : ");
+		char ch = sc.next().charAt(0);
+		// 4. 몇번째 인덱스에 있는지 몇개 있는지 출력
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] == ch) {
+				System.out.printf("%d ", i);
+				cnt++;
+			}
+		}
+		System.out.printf("\n%c 개수 : %d", ch, cnt);
+	}
+	
+	public void practice08() {
+		System.out.print("주민등록번호(-포함) : ");
+		String id = sc.next();
+		
+		char idArr[] = id.toCharArray();
+		
+		for(int i=0; i<idArr.length; i++) {
+			if(i>7) {
+				System.out.print("*");
+			}
+			else {
+				System.out.printf("%c", idArr[i]);
+			}
+		}
+	}
+	
+	public void practice09() {
+		int arr[] = new int[10];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = (int)(Math.random() * 10 + 1);
+		}
+		
+		System.out.println(Arrays.toString(arr));
+		Arrays.sort(arr);
+		System.out.printf("최댓값 : %d\n최솟값 : %d", arr[arr.length-1], arr[0]);
+	}
+	
+	public void practice10() {
+		int rand;
+		int arr[] = new int[10];
+			
+		for(int i=0; i<arr.length; i++) {
+			rand = (int)(Math.random() * 10 + 1);
+			
+			for(int j=0; j<=i; j++) {
+				if(i==j) {
+					arr[i] = rand;
+					System.out.printf("%d ", arr[i]);
+				}
+				else if(arr[j] == rand) {
+					i--;
+					break;
+				}
+			}
+			
+		}
+	}
+	
+	public void practice11() {
+		int n;
+		int arr[];
+		int tmp;
+		
+		while(true) {
+			System.out.print("정수 : ");
+			n = sc.nextInt();
+			tmp = n/2+1;
+			
+			arr = new int[n];
+			
+			if(n%2==0 || n<3) {
+				System.out.println("다시 입력하세요.");
+				continue;
+			}
+			
+			for(int i=0; i<n; i++) {
+				if(i<=tmp-1) {
+					arr[i] = i+1;					
+				}
+				if(i>tmp-1) {
+					arr[i] = --tmp;
+				}
+				System.out.printf("%d ", arr[i]);
+			}
+			
+			break;
+		}
+	}
+	
+	public void practice12() {
+		System.out.println("배열의 크기를 입력하세요 : ");
+		int n = sc.nextInt();
+		char q;
+		int index = 0;
+		
+		String arr[] = new String[n];
+		
+		
+		
+		while(true) {
+			sc.nextLine();
+			
+			for(int i=index; i<index+n; i++) {
+				System.out.printf("%d번째 문자열 : ", i+1);
+				arr[i] = sc.nextLine();
+			}
+			
+			index = arr.length;
+			
+			System.out.print("값을 더 입력하시겠습니까?(Y/N) : ");
+			q = sc.next().charAt(0);
+			
+			if(q=='N'|| q=='n') {
+				System.out.println(Arrays.toString(arr));
+				return;
+			}
+			
+			System.out.print("더 입력하고 싶은 개수 : ");
+			n = sc.nextInt();
+			
+			arr = Arrays.copyOf(arr, arr.length+n);
+		}
+	}
+	
+}
---
+++ 05_Array/src/com/kh/array/practice/Run.java
@@ -0,0 +1,22 @@
+package com.kh.array.practice;
+
+public class Run {
+
+	public static void main(String[] args) {
+		ArrayPractice ap = new ArrayPractice();
+		
+//		ap.practice01();
+//		ap.practice02();
+//		ap.practice03();
+//		ap.practice04();
+//		ap.practice05();
+//		ap.practice06();
+//		ap.practice07();
+//		ap.practice08();
+//		ap.practice09();
+//		ap.practice10();
+//		ap.practice11();
+		ap.practice12();
+	}
+
+}
